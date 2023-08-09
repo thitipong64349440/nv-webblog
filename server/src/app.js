5a44134e1c5524b3bed920d41239427420215358
thitipong64349440 <thitipongr64@nu.ac.th>
2023-08-09 12:45:24 +0700

    folder perpared

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,21 @@
-console.log('hello')
+let express = require('express')
+
+const app = express()
+
+ 
+
+app.get('/status', function (req,res){
+
+    res.send('Hello nodejs server')
+
+})
+
+ 
+
+let port = 8081
+
+app.listen(port, function(){
+
+    console.log('server running on ' + port)
+
+})
