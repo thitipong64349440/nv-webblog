cdd56d599f4906b74397ce34ff7309cb6ececeb0
thitipong64349440 <141795216+thitipong64349440@users.noreply.github.com>
2023-08-10 10:36:21 +0700

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -6,24 +6,15 @@ const app = express()
 
 app.get('/status', function (req,res){
 
-    res.send('Hello nodejs server')
+    res.send('Hello nodejs server i am max')
 
 })
 
-app.get('/hello/:person', function (req,res) {
-    console.log('hello - ' + req.params.person)
-    res.send('sey hello with ' + req.params.person)
-   })
 
-   // get user by id
-app.get('/user/:userId', function (req, res) {
-    res.send('ดูข้อมูลผู้ใช้งาน')
-   })
-   // get all user
-   app.get('/users', function (req, res) {
-    res.send('เรียกข้อมูลผใู้ชง้านทั้งงัหมด')
-   })
-
+app.get('/hello/:name', function (req,res) {
+    console.log('hello - ' + req.params.name)
+    res.send('sey hello with ' + req.params.name)
+})
 
 let port = 8081
 
